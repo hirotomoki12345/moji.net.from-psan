5b5e7421db4d23adde7f111b8a6edf51e952ddc6
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-07-25 09:11:40 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-function startTranscription() {
-  const imageInput = document.getElementById('imageInput');
-  const resultDiv = document.getElementById('result');
-  const file = imageInput.files[0];
 
-  if (!file) return;
-
-  Tesseract.recognize(
-    file,
-    'eng', // 英語の文字起こしを行う場合
-    { logger: m => console.log(m) }
-  ).then(({ data: { text } }) => {
-    resultDiv.textContent = text;
-  });
-}
